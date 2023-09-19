008f3edbd359370bf4e5e7b8b4322045e8b235c1
Bong <songning.lan@gmail.com>
2023-09-19 19:49:19 -0400

    save

--- Board.py
+++ Board.py
@@ -18,10 +18,27 @@ class TicTacToeBoard(tk.Tk):
         grid_frame.pack()
         for row in range(3):
 '''
-
-
-
-    
-
-def TurnCheck():
-    pass
+'''
+def init_board(frame:tk.Frame):
+    for row in range(3):
+        frame.rowconfigure(row, weight=1, minsize=50)
+        frame.columnconfigure(row, weight=1, minsize=75)
+        for col in range(3):
+                button = tk.Button(
+                    master=frame,
+                    text="",
+                    font=font.Font(size=36, weight="bold"),
+                    fg="black",
+                    width=3,
+                    height=2,
+                    highlightbackground="lightblue",
+                )
+                frame.grid._cells[button] = (row, col)
+                button.grid(
+                    row=row,
+                    column=col,
+                    padx=5,
+                    pady=5,
+                    sticky="nsew"
+                )
+'''
